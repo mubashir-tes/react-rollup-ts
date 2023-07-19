6528ac2b77217e5abc920e389842e6962f96a7a1
mubashir-tes <mubashir.official@gmail.com>
2023-07-19 22:09:19 +0500

    resolve build issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
-import babel from "@rollup/plugin-babel";
+import { babel } from "@rollup/plugin-babel";
 import image from "@rollup/plugin-image";
 import typescript from "@rollup/plugin-typescript";
 import { readFileSync } from "fs";
-import dts from "rollup-plugin-dts";
 import includePaths from "rollup-plugin-includepaths";
 import json from "rollup-plugin-json";
 
@@ -11,15 +10,15 @@ const pkg = JSON.parse(
   readFileSync(new URL("./package.json", import.meta.url), "utf8")
 );
 export default [
-  {
-    input: "src/index.d.ts",
-  },
-  {
-    output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
-    ],
-  },
+  // {
+  //   input: "src/index.d.ts",
+  // },
+  // {
+  //   output: [
+  //     { file: pkg.types, format: "cjs" },
+  //     { file: pkg.types, format: "es" },
+  //   ],
+  // },
   {
     input: "src/index.tsx",
     external: [
@@ -27,7 +26,7 @@ export default [
       ...Object.keys(pkg?.dependencies),
     ],
     plugins: [
-      dts(),
+      // dts(),
       includePaths({
         paths: ["src"],
         extensions: [".js", ".ts", ".tsx", ".jsx"],
--- src/container/index.tsx
+++ src/container/index.tsx
@@ -1,8 +1,10 @@
+import { Alert } from "antd";
 function Button() {
   console.debug("running");
   return (
     <div>
       <button>Button</button>
+      <Alert message="Hello World" />
     </div>
   );
 }
